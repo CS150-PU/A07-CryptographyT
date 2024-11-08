06878764edd4f746d7280c6d066f76f7c7b1b723
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2024-11-08 04:04:01 +0000

    Updated template

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 //******************************************************************************
 // File name:   main.cpp
-// Author:      CS Faculty
-// Date:        xx/xx/xxxx (Replace xx/xx/xxxx with month/day/year)
-// Class:       CS150-xx (Replace xx with your section number)
-// Assignment:  Hello World
-// Purpose:     Display the message Hello World to the screen
-// Hours:       0.25
+// Author:      
+// Date:        
+// Class:       
+// Assignment:  
+// Purpose:     
+// Hours:       
 //******************************************************************************
 
 #include <iostream>
